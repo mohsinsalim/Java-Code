840217329d08188a15cde5e3d5a06f5b07051de3
mohxi <mohxi@DESKTOP-C928T7K.fios-router.home>
2020-05-23 16:06:39 -0400

    First Commit

---
+++ Java Tutorials/src/java_lesson_02/BasicArray.java
@@ -0,0 +1,19 @@
+package java_lesson_02;
+
+public class BasicArray {
+
+	public static void main(String[] args) {
+		
+		// int array
+		
+		int[] array = new int[5];
+		array[0] = 28;
+		array[1] = 33;
+		
+		for(int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+
+	}
+
+}
