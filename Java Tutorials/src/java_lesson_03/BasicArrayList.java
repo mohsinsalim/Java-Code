e8d7c8052a6132a39fe81680332594851e759525
mohxi <mohxi@Mohsin.fios-router.home>
2020-05-27 23:04:20 -0400

    Test Commit

---
+++ Java Tutorials/src/java_lesson_03/BasicArrayList.java
@@ -0,0 +1,49 @@
+package java_lesson_03;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BasicArrayList {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> array = getArray();
+		
+		System.out.println("The output is: \n");
+		for(int i = 0; i < array.size(); i++) {
+			System.out.println("Array[" + i + "] = " + array.get(i));
+		}
+		
+
+	}
+	
+	static ArrayList<String> getArray() {
+		
+		ArrayList<String> array = new ArrayList<String>();
+		System.out.println("Enter string here,");
+		System.out.println("Terminate with empty line");
+		
+		Scanner input = new Scanner(System.in);
+		
+		while(input.hasNextLine()) {
+			
+			String line = input.nextLine();
+			
+			if(line.equals("")) {
+				System.out.println("Terminated...");
+				break;
+			}
+			else {
+				array.add(line);
+			}
+	
+		}
+		
+		//Scanner close
+		//input.close();
+		
+		
+		return array;
+	}
+
+}
